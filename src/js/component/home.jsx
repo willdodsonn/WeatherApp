f92704754329f950d01ee412558e0822652f89a2
willdodsonn <sodlax38@gmail.com>
2022-03-10 14:52:02 +0000

    fuck apis there horseshit i hate them

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,36 @@
-import React from "react";
-
+import React, { useState } from "react";
+import axios from "axios";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	// const url = 'https://api.openweathermap.org/data/2.5/weather?q=orlando&appid=fafc29b620c662ec7dc94a3e4b9014fa';
+
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="home">
+			<div className="container">
+				<div className="top">
+					<div className="location">
+						<p>currentLocation</p>
+					</div>
+					<div className="temp">
+						<h1>44°F</h1>
+					</div>
+					<div className="description">
+						<p>Clouds</p>
+					</div>
+					<div className="bottom">
+						<div className="feels">
+							<p>43°F</p>
+						</div>
+						<div className="humidity">
+							<p>20%</p>
+						</div>
+						<div className="wind">1 MPH</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
